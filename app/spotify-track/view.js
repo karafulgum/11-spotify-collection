7d7f92261bf06e1a62eeb6d0b8d86f2878a42062
Kara Fulgum <karafulgum@gmail.com>
2016-10-17 16:14:21 -0500

    Added stuff to index.html

--- app/spotify-track/view.js
+++ app/spotify-track/view.js
@@ -9,9 +9,9 @@ export default class view {
     this.track.classList.add('panel-block', 'track');
     this.track.innerHTML = `
     <div class="panel-block track">
-    <img src="" alt="">
+    <img class="track__img" src="" alt="">
     <h2 class="track__artist"></h2>
-    <h4 class="track__name"></h4>
+    <h4 class="track__name"><a class="track__link" href=""></a></h4>
     </div>`;
   }
   render() {
